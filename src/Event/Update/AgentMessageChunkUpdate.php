235cec6d0592c16306e254464cc8a89e4bc782c9
yankewei <yankewei1993@gmail.com>
2026-06-13 23:20:25 +0800

    Add AgentMessageChunkUpdate DTO

---
+++ src/Event/Update/AgentMessageChunkUpdate.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yankewei\AcpClient\Event\Update;
+
+use Yankewei\AcpClient\Exception\AcpException;
+use Yankewei\AcpClient\Util\Assert;
+
+final class AgentMessageChunkUpdate implements SessionUpdate
+{
+    /**
+     * @param array<string, mixed> $content
+     */
+    public function __construct(
+        private readonly string $sessionId,
+        private readonly ?string $messageId,
+        private readonly array $content,
+    ) {
+    }
+
+    /**
+     * @param array<string, mixed> $update
+     *
+     * @throws AcpException
+     */
+    public static function fromUpdate(string $sessionId, array $update): self
+    {
+        if (($update['sessionUpdate'] ?? null) !== 'agent_message_chunk') {
+            throw new AcpException('Invalid agent_message_chunk update: sessionUpdate must be agent_message_chunk');
+        }
+
+        return new self(
+            $sessionId,
+            Assert::optionalString(
+                $update,
+                'messageId',
+                'Invalid agent_message_chunk update: messageId must be a string or null',
+            ),
+            Assert::object(
+                $update['content'] ?? null,
+                'Invalid agent_message_chunk update: content must be an object',
+            ),
+        );
+    }
+
+    public function getSessionId(): string
+    {
+        return $this->sessionId;
+    }
+
+    public function getUpdateType(): string
+    {
+        return 'agent_message_chunk';
+    }
+
+    public function getMessageId(): ?string
+    {
+        return $this->messageId;
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function getContent(): array
+    {
+        return $this->content;
+    }
+}
---
+++ tests/Event/Update/AgentMessageChunkUpdateTest.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yankewei\AcpClient\Tests\Event\Update;
+
+use PHPUnit\Framework\TestCase;
+use Yankewei\AcpClient\Event\Update\AgentMessageChunkUpdate;
+use Yankewei\AcpClient\Event\Update\SessionUpdate;
+use Yankewei\AcpClient\Exception\AcpException;
+
+final class AgentMessageChunkUpdateTest extends TestCase
+{
+    public function testParsesTextChunk(): void
+    {
+        $update = AgentMessageChunkUpdate::fromUpdate('sess_1', [
+            'sessionUpdate' => 'agent_message_chunk',
+            'messageId' => 'msg_1',
+            'content' => ['type' => 'text', 'text' => 'Hello'],
+        ]);
+
+        self::assertInstanceOf(SessionUpdate::class, $update);
+        self::assertSame('sess_1', $update->getSessionId());
+        self::assertSame('agent_message_chunk', $update->getUpdateType());
+        self::assertSame('msg_1', $update->getMessageId());
+        self::assertSame(['type' => 'text', 'text' => 'Hello'], $update->getContent());
+    }
+
+    public function testMessageIdIsOptional(): void
+    {
+        $update = AgentMessageChunkUpdate::fromUpdate('sess_1', [
+            'sessionUpdate' => 'agent_message_chunk',
+            'content' => ['type' => 'text', 'text' => 'Hello'],
+        ]);
+
+        self::assertNull($update->getMessageId());
+    }
+
+    public function testRejectsWrongDiscriminator(): void
+    {
+        $this->expectException(AcpException::class);
+
+        AgentMessageChunkUpdate::fromUpdate('sess_1', ['sessionUpdate' => 'tool_call']);
+    }
+
+    public function testRejectsMissingContent(): void
+    {
+        $this->expectException(AcpException::class);
+        $this->expectExceptionMessage('Invalid agent_message_chunk update: content must be an object');
+
+        AgentMessageChunkUpdate::fromUpdate('sess_1', ['sessionUpdate' => 'agent_message_chunk']);
+    }
+
+    public function testRejectsListContent(): void
+    {
+        $this->expectException(AcpException::class);
+        $this->expectExceptionMessage('Invalid agent_message_chunk update: content must be an object');
+
+        AgentMessageChunkUpdate::fromUpdate('sess_1', [
+            'sessionUpdate' => 'agent_message_chunk',
+            'content' => [['type' => 'text', 'text' => 'Hello']],
+        ]);
+    }
+
+    public function testRejectsInvalidMessageIdType(): void
+    {
+        $this->expectException(AcpException::class);
+        $this->expectExceptionMessage('Invalid agent_message_chunk update: messageId must be a string or null');
+
+        AgentMessageChunkUpdate::fromUpdate('sess_1', [
+            'sessionUpdate' => 'agent_message_chunk',
+            'messageId' => 123,
+            'content' => ['type' => 'text', 'text' => 'Hello'],
+        ]);
+    }
+}
